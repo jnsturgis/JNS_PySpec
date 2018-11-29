a34b635ca4d6f3e9c2e55deb0db427e0386ab48d
James Sturgis <james.sturgis@univ-amu.fr>
2018-11-29 18:00:18 +0100

    xy file handling is better, with error checking

--- main.py
+++ main.py
@@ -6,17 +6,14 @@
 #   save the resulting figure in different formats (thanks to matplotlib).
 #
 #   First project represents a PySpec viewer and converter.
-#   TODO Handle file parsing problems and other error handling
 #   TODO Form for spectrum properties to allow creation of PySpec format
 #   TODO Parsers for different commonly found filetypes.
 #   TODO Abstractions of current objects Dataset, Window, App
 #   TODO Documentation of program pydoc and doxygen and Github wiki
-#   TODO Save and SaveAs dialogs to write as different formats.
-#   TODO Parse filename to path+filename+type
 
+from PyQt5 import QtWidgets
 import sys
 import os
-from PyQt5 import QtWidgets
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -27,6 +24,7 @@ def TestComment(my_string):
 
 class Dataset:
     '''A dataset class, datasets should know how to fill themselves from a file, write themselves, draw themselves and so on'''
+    # This class should not really know much about the GUI or OS
     x_values = []
     y_values = []
     x_label = "Wavelength (nm)"
@@ -36,12 +34,7 @@ class Dataset:
     
     def __init__(self, source=None,  name=None,  type='xy' ):
         
-        while len(self.x_values):               # Empty class variables I do not really see why it is necessary.
-            self.x_values.pop()                     # Since I did not intend to reuse the same instance each time.
-        while len(self.y_values):               # But it is probably good form to make sure we know where things
-            self.y_values.pop()                     # start.
-        self.x_label = "Wavelength (nm)"
-        self.y_label = "Absorption"
+        self.valInit()
         self.filename = name
         self.filetype = type
         
@@ -49,14 +42,50 @@ class Dataset:
                                                                 # This section should really switch on the filetype
                                                                 # and check the filetype declared corresponds to
                                                                 # the declaration.
+            lineNumber = 0
             for line in source:
-                if TestComment(line):
-                    pass
+                lineNumber+= 1
+                if TestComment(line):               # Ignore comments that start with a '#'
+                    continue
                 else:
                     # Read in the data
-                    self.x_values.append(float(line.split()[0]))
-                    self.y_values.append(float(line.split()[1]))
+                    words = line.split()
+                    if len(words)==0:               # Ignore blank lines too
+                        continue
+                    elif (len(words)!=2):
+                        if self.fileReadError(lineNumber, "2 numbers"):
+                            break
+                        else:
+                            continue
+                    else:       # xy pair all is well (if they are numbers)
+                        try:
+                            self.x_values.append(float(words[0]))
+                            self.y_values.append(float(words[1]))
+                        except:
+                            if self.fileReadError(lineNumber, "2 numbers"):
+                                break
+                            else:
+                                continue
 
+    def fileReadError(self, lineNumber, expected):
+        msg = QtWidgets.QMessageBox.question(None,"Error", "Error reading file!\nLine #"+str(lineNumber)+"does not contain"+expected, QtWidgets.QMessageBox.Ignore | QtWidgets.QMessageBox.Abort)
+        if msg == QtWidgets.QMessageBox.Abort:
+            self.valInit()
+            return True
+        else:
+            return False
+
+    def valInit(self):
+        '''Initialize the values of the instance'''
+        while len(self.x_values):               # Empty class variables I do not really see why it is necessary.
+            self.x_values.pop()                     # Since I did not intend to reuse the same instance each time.
+        while len(self.y_values):               # But it is probably good form to make sure we know where things
+            self.y_values.pop()                     # start.
+        self.x_label = "Wavelength (nm)"
+        self.y_label = "Absorption"
+        self.filename = None
+        self.filetype = "xy"
+        
     def write(self,  dest=None,  type=None):
         '''Write the datastructure to the destination using the syntax associated with the type'''
         # If type is none use saved type or if there is none xy format (for the moment).
@@ -74,7 +103,7 @@ class Dataset:
             axis.set(xlabel=self.x_label, ylabel=self.y_label, Title="No Data")
 
 class Window(QtWidgets.QDialog):
-    
+    '''The main window for the application'''
     data = Dataset()
     
     def __init__(self, parent=None):
@@ -162,6 +191,12 @@ class Window(QtWidgets.QDialog):
             self.plot()
     
     def edit(self):
+        print ("  Filename :" + self.data.filename)
+        print ("  Filetype :" + self.data.filetype)
+        print ("Datapoints :" + str(len(self.data.x_values)))
+        print ("   X label :" + self.data.x_label)
+        print ("   Y label :" + self.data.y_label)
+        #if OK pressed copy to data and replot
         self.plot()
     
 if __name__ == '__main__':
